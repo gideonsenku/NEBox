7848d15b006561f48f465021c2305e51af200497
GideonSenku <gentlewind2391@gmail.com>
2024-07-03 17:20:12 +0800

    Initial Commit

---
+++ BoxJs/BoxJsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxJsApp.swift
+//  BoxJs
+//
+//  Created by Senku on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BoxJsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
